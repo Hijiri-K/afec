799a94c47922f4e10086a07cade4c7d4d269045f
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-10-20 16:59:25 +0900

    error fix 6 test(stop checkoffer)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -253,19 +253,19 @@ $.ajax({
       });
 
 
-  $(function(){
-      setInterval(function(){
-        $.ajax({
-            url: "/posts/checkoffer",
-            type: "post",
-
-            success: function(responce) {
-              // alert("ok");
-            },
-            error: function(xhr) {
-              // alert("error");
-            }
-        });
-      },5000);
-  });
+  // $(function(){
+  //     setInterval(function(){
+  //       $.ajax({
+  //           url: "/posts/checkoffer",
+  //           type: "post",
+  //
+  //           success: function(responce) {
+  //             // alert("ok");
+  //           },
+  //           error: function(xhr) {
+  //             // alert("error");
+  //           }
+  //       });
+  //     },5000);
+  // });
 });
