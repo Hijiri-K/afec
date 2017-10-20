d0b58488f46fa5708d224c8f680c17c1e8f00a57
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-10-20 19:45:49 +0900

    error fix6 (set csrf_token except)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery :except => [:offer]#with: :exception
 
   before_action :set_current_user
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
-  
+
   end
 
   def authenticate_user
