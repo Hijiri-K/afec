61a5b736ec22c6b723d00a144c1a06733bab2cb9
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-10-25 23:14:09 +0900

    add cal action in post_controller for removing calculation codes from index view and make app easy to maintain and simple codes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,8 +12,41 @@ class HomeController < ApplicationController
   def test
   end
 
+
   def test2
+    if Post.find_by(user_id: @current_user.id)
+        @mypost = Post.find_by(user_id: @current_user.id)
+        @posts = Post.where(currency_have: @mypost.currency_want, currency_want: @mypost.currency_have)
+    else
+      @posts = Post.all.order(created_at: :desc)
+    end
+
+    if Post.find_by(user_id: @current_user.id)
+      require 'json'
+      @mypost = Post.find_by(user_id: @current_user.id)
+      if File.exist?("tmp/#{@mypost.id}.json")
+        File.open("tmp/#{@mypost.id}.json", 'r') do |f|
+        offers = JSON.load(f)
+        @checkoffer = offers.fetch("offer_from")
+        # @offer_to = offers.fetch("offer_to")
+        end
+      end
+
+      if File.exist?("tmp/accept#{@mypost.id}.json")
+        File.open("tmp/accept#{@mypost.id}.json", 'r') do |f|
+        offers = JSON.load(f)
+        @acceptcheckoffer = offers.fetch("offer_from")
+        # @acceptoffer_to = offers.fetch("offer_to")
+        end
+      end
+
+      if File.exist?("tmp/map#{@mypost.id}.json")
+        @mapshow = true
+      end
+    end
   end
-
-
 end
+
+
+  # <!-- 計算 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+      # <!-- 距離の計算 -->
