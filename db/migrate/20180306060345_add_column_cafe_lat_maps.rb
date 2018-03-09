9e91dd9ca8efee9f060692a500db3b35526dd890
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-03-09 20:45:34 +0900

    migration fix

--- db/migrate/20180306060345_add_column_cafe_lat_maps.rb
+++ db/migrate/20180306060345_add_column_cafe_lat_maps.rb
@@ -1,7 +1,6 @@
 class AddColumnCafeLatMaps < ActiveRecord::Migration[5.1]
   def change
     remove_column :maps, :cafe_lat_lng, :float
-    remove_column :maps, :cafe_lat_lng, :float
     add_column :maps, :cafe_lat, :float
     add_column :maps, :cafe_lng, :float
     add_column :maps, :center_lat, :float
