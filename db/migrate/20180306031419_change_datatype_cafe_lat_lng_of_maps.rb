213942068977e4e820ed7b9a7a765eaec7060363
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-03-09 20:36:25 +0900

    migration fix

--- db/migrate/20180306031419_change_datatype_cafe_lat_lng_of_maps.rb
+++ db/migrate/20180306031419_change_datatype_cafe_lat_lng_of_maps.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeCafeLatLngOfMaps < ActiveRecord::Migration[5.1]
   def change
-    change_column :maps, :cafe_lat_lng, :float, array:true
-    change_column :maps, :center_lat_lng, :float, array:true
+    # change_column :maps, :cafe_lat_lng, :float, array:true
+    # change_column :maps, :center_lat_lng, :float, array:true
   end
 end
