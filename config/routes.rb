02b5854be06831dc897f3615171c2ac569fbc603
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-03-22 14:52:21 +0900

    after signup redirect fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: "posts#index"
 
   # post "likes/:id/create" => "likes#create"
